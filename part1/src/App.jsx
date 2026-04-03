2fb6c0e1689eb81dc3fb550dcc66b69ddae6ccba
Rust <nexousrust@gmail.com>
2026-04-03 20:36:39 +0500

    part 1 exercise 5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -39,30 +39,32 @@ const Total    = (props) => {
 }
 
 function App() {
-  const course = 'Half Stack application development'
- const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header title={course}/>
-      <Content  part1={parts[0].name} exercises1={parts[0].exercises} 
-      part2={parts[1].name} exercises2={parts[1].exercises}
-      part3={parts[2].name} exercises3={parts[2].exercises}/>
+      <Header title={course.name}/>
+      <Content  part1={course.parts[0].name} exercises1={course.parts[0].exercises} 
+      part2={course.parts[1].name} exercises2={course.parts[1].exercises}
+      part3={course.parts[2].name} exercises3={course.parts[2].exercises}/>
       
-      <Total number={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Total number={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
     </>
   )
 }
