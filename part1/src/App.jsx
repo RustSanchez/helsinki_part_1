8d9381e567c82eb532514d4a4903278e06255edd
Rust <nexousrust@gmail.com>
2026-04-10 22:55:49 +0500

    part 1 exercise 6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,71 +1,42 @@
 import { useState } from 'react'
+import './index.css'
 
 
-
-const Header  = (props) => {
+const Display  = (props) => {
   console.log(props)
   return (
     <div>
-      <p>{props.title}</p>
+      <p className={props.className}>{props.title}</p>
     </div>
   )
 }
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
-const Part   = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>{props.part_name} {props.part_number}</p>
-    </div>
-  )
-} 
-const Content   = (props)=>{
-  console.log(props)
-  return (
-    <div>
-      <Part part_name={props.part1} part_number={props.exercises1}/>
-      <Part part_name={props.part2} part_number={props.exercises2}/>
-      <Part part_name={props.part3} part_number={props.exercises3}/>
-    </div>
-  )
-}
-const Total    = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>{props.number}</p>
-    </div>
-  )
-}
 
 function App() {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const title1= 'give feedback'
+  const title2 = 'statistics'
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
 
   return (
     <>
-      <Header title={course.name}/>
-      <Content  part1={course.parts[0].name} exercises1={course.parts[0].exercises} 
-      part2={course.parts[1].name} exercises2={course.parts[1].exercises}
-      part3={course.parts[2].name} exercises3={course.parts[2].exercises}/>
-      
-      <Total number={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
-    </>
+      <Display className='title' title={title1}/>
+      <Button onClick={() => setGood(good + 1)} text='good'/>
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral'/>
+      <Button onClick={() => setBad(bad + 1)} text='bad'/>
+
+      <Display className='title' title={title2}/>
+      <Display title={`good: ${good}`}/>
+      <Display title={`neutral: ${neutral}`}/>
+      <Display title={`bad: ${bad}`}/>
+    </> 
   )
 }
 
